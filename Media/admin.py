f1253c49fa10ec1c98b0d64c4da9da09dc5158a5
Ostap <ostapbojko04@gmail.com>
2024-11-19 20:52:12 +0200

    add Media/admin.py

--- Media/admin.py
+++ Media/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from Media.models import HashTag, Post, Comment, Like, PostImage
+
+
+class ImageInline(admin.StackedInline):
+    model = PostImage
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = (ImageInline,)
+
+
+admin.site.register(HashTag)
+admin.site.register(Comment)
+admin.site.register(Like)
