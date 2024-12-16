0cb1b66afa3758d46abcba7f098cc9a5c9127067
Ostap <ostapbojko04@gmail.com>
2024-12-16 20:26:56 +0200

    Edit DATABASE

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -83,9 +83,13 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Social_Media_Api",
+        "USER": "Social_Media_User",
+        "PASSWORD": "social_media_password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
